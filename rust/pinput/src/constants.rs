83bcedb7eefb28272420e824d4ee1c41d89e508c
Vyr Cossont <VyrCossont@users.noreply.github.com>
2021-11-17 01:45:44 -0800

    Rust: keep game controllers open and update state every loop
    For some reason, this makes triggers work. It also seems like the right way, but the wrong way was working… except for triggers.

--- rust/pinput/src/constants.rs
+++ rust/pinput/src/constants.rs
@@ -1,8 +1,10 @@
 use uuid::Uuid;
 
 /// Magic byte sequence we used to identify Pinput-enabled cartridges.
-pub static PINPUT_MAGIC: Uuid = Uuid::from_u128(0x0220c74677ab446ebedc7fd6d277984d);
+pub const PINPUT_MAGIC: Uuid = Uuid::from_u128(0x0220c74677ab446ebedc7fd6d277984d);
 
 /// Pinput can fit this many gamepads into the GPIO area.
-pub static PINPUT_MAX_GAMEPADS: usize = 8;
+pub const PINPUT_MAX_GAMEPADS: usize = 8;
 
+/// 60 FPS.
+pub static FRAME_DURATION_MS: u16 = 16;
--- rust/pinput/src/main.rs
+++ rust/pinput/src/main.rs
@@ -1,7 +1,7 @@
 use ctrlc;
 use sdl2;
-use sdl2::controller::{Axis, Button};
-use sdl2::joystick::PowerLevel;
+use sdl2::controller::{Axis, Button, GameController};
+use sdl2::joystick::{Joystick, PowerLevel};
 use std::cmp::min;
 use std::sync::Arc;
 use std::sync::mpsc::channel;
@@ -14,9 +14,9 @@ mod pico8_connection;
 mod constants;
 mod gamepad;
 
-use constants::{PINPUT_MAGIC, PINPUT_MAX_GAMEPADS};
+use constants::{FRAME_DURATION_MS, PINPUT_MAGIC, PINPUT_MAX_GAMEPADS};
 use pico8_connection::Pico8Connection;
-use gamepad::{PinputGamepadFlags, PinputGamepadButtons, PinputGamepadArray};
+use gamepad::{PinputGamepad, PinputGamepadFlags, PinputGamepadButtons, PinputGamepadArray};
 
 #[derive(thiserror::Error, Debug)]
 enum Error {
@@ -59,6 +59,14 @@ const SDL_GAME_CONTROLLER_BUTTONS: [Button; 15] = [
     Button::DPadRight,
 ];
 
+/// SDL has two subsystems for accessing parts of the same device.
+struct SdlGamepad {
+    /// Used to check the power level.
+    joystick: Joystick,
+    /// Used for everything else.
+    game_controller: GameController,
+}
+
 fn main() -> Result<(), Error> {
     let keep_going = Arc::new(AtomicBool::new(true));
     let keep_going_ctrlc = keep_going.clone();
@@ -87,18 +95,22 @@ fn main() -> Result<(), Error> {
     let pico8_connection = Pico8Connection::try_new()?;
     println!("connected: PID {}", pico8_connection.pid);
 
-    let frame_duration = Duration::microseconds(16667);
     let (timer_tx, timer_rx) = channel();
     let timer = Timer::new();
     let _timer_guard = Some(timer.schedule_repeating(
-        frame_duration,
+        Duration::milliseconds(FRAME_DURATION_MS as i64),
         move || timer_tx.send(())
             .expect("we should always be able to send timer ticks")
     ));
 
     let mut gamepads: PinputGamepadArray;
+    let mut sdl_gamepads: [Option<SdlGamepad>; PINPUT_MAX_GAMEPADS] = Default::default();
     while keep_going.load(Ordering::Relaxed) {
         timer_rx.recv()?;
+
+        // Updates the state of all game controllers.
+        // We could also run the SDL event loop, which would call this automatically.
+        game_controller_subsystem.update();
 
         let magic = pico8_connection.gpio_as_uuid.read()?;
         if magic == PINPUT_MAGIC {
@@ -110,73 +122,89 @@ fn main() -> Result<(), Error> {
         let sdl_num_joysticks = game_controller_subsystem.num_joysticks()
             .map_err(|s| Error::SdlStringError(s))?;
         for gamepad_index in 0..min(sdl_num_joysticks as usize, PINPUT_MAX_GAMEPADS) {
-            let joystick_index = gamepad_index as u32;
-            if !game_controller_subsystem.is_game_controller(joystick_index) {
-                continue;
-            }
-
-            // TODO: should we be getting joysticks by index or by `SDL_JoystickInstanceID`?
-            // TODO: should we be opening them every frame or just once?
-            let joystick = joystick_subsystem.open(joystick_index)?;
-            let mut game_controller = game_controller_subsystem.open(joystick_index)?;
-            let gamepad = &mut gamepads[gamepad_index];
-
-            // TODO: test rumble
-            game_controller.set_rumble(
-                ((gamepad.lo_freq_rumble as f64) / (u8::MAX as f64) * (u16::MAX as f64)) as u16,
-                ((gamepad.hi_freq_rumble as f64) / (u8::MAX as f64) * (u16::MAX as f64)) as u16,
-                frame_duration.num_milliseconds() as u32
-            )?;
-
-            // Read gamepad capabilities and power level.
-            gamepad.flags = PinputGamepadFlags::default();
-            gamepad.flags.insert(PinputGamepadFlags::CONNECTED);
-            // The SDL game controller API allows access to the Guide button, in theory.
-            // TODO: does this work with XInput controllers on Windows?
-            gamepad.flags.insert(PinputGamepadFlags::HAS_GUIDE_BUTTON);
-            // SDL doesn't currently have a way to tell if a gamepad is charging.
-            let power_level = joystick.power_level()?;
-            gamepad.flags.insert(PinputGamepadFlags::from(power_level));
-            match power_level {
-                PowerLevel::Low => {
-                    gamepad.battery = u8::MAX / 3;
-                },
-                PowerLevel::Medium => {
-                    gamepad.battery = u8::MAX / 3 * 2;
-                },
-                PowerLevel::Full => {
-                    gamepad.battery = u8::MAX;
-                },
-                _ => {
-                    gamepad.battery = 0;
-                },
-            }
-
-            // Read gamepad buttons.
-            // Temporary variable used to avoid an unaligned access.
-            let mut buttons = PinputGamepadButtons::default();
-            for button in SDL_GAME_CONTROLLER_BUTTONS {
-                if game_controller.button(button) {
-                    if let Ok(button) = PinputGamepadButtons::try_from(button) {
-                        buttons.insert(button);
-                    }
+            if sdl_gamepads[gamepad_index].is_none() {
+                let sdl_gamepad_index = gamepad_index as u32;
+                if !game_controller_subsystem.is_game_controller(sdl_gamepad_index) {
+                    continue;
                 }
+                sdl_gamepads[gamepad_index] = Some(SdlGamepad {
+                    joystick: joystick_subsystem.open(sdl_gamepad_index)?,
+                    game_controller: game_controller_subsystem.open(sdl_gamepad_index)?,
+                });
             }
-            gamepad.buttons = buttons;
 
-            // Read gamepad axes (including triggers).
-            // Note that SDL Y axes are upside-down compared to XInput:
-            // <https://github.com/libsdl-org/SDL/blob/9130f7c/src/joystick/windows/SDL_xinputjoystick.c#L462-L465>
-            gamepad.left_stick_x = game_controller.axis(Axis::LeftX);
-            gamepad.left_stick_y = !game_controller.axis(Axis::LeftY);
-            gamepad.right_stick_x = game_controller.axis(Axis::RightX);
-            gamepad.right_stick_y = !game_controller.axis(Axis::RightY);
-            gamepad.left_trigger = (game_controller.axis(Axis::TriggerLeft) / 0x80) as u8;
-            gamepad.right_trigger = (game_controller.axis(Axis::TriggerRight) / 0x80) as u8;
+            let mut gamepad = &mut gamepads[gamepad_index];
+            if let Some(sdl_gamepad) = &mut sdl_gamepads[gamepad_index] {
+                sync_gamepad(sdl_gamepad, &mut gamepad)?;
+            }
         }
 
         pico8_connection.gpio_as_gamepads.write(&gamepads)?;
     }
+
+    Ok(())
+}
+
+fn sync_gamepad(sdl_gamepad: &mut SdlGamepad, gamepad: &mut PinputGamepad) -> Result<(), Error> {
+    let game_controller = &mut sdl_gamepad.game_controller;
+    let joystick = &sdl_gamepad.joystick;
+
+    if !game_controller.attached() {
+        *gamepad = PinputGamepad::default();
+        return Ok(())
+    }
+
+    // TODO: test rumble. Some devices (Xbone on Windows?) claim not to support it.
+    game_controller.set_rumble(
+        ((gamepad.lo_freq_rumble as f64) / (u8::MAX as f64) * (u16::MAX as f64)) as u16,
+        ((gamepad.hi_freq_rumble as f64) / (u8::MAX as f64) * (u16::MAX as f64)) as u16,
+        FRAME_DURATION_MS as u32
+    )?;
+
+    // Read gamepad capabilities and power level.
+    gamepad.flags = PinputGamepadFlags::default();
+    gamepad.flags.insert(PinputGamepadFlags::CONNECTED);
+    // The SDL game controller API allows access to the Guide button, even through XInput.
+    gamepad.flags.insert(PinputGamepadFlags::HAS_GUIDE_BUTTON);
+    // SDL doesn't currently have a way to tell if a gamepad is charging.
+    let power_level = joystick.power_level()?;
+    gamepad.flags.insert(PinputGamepadFlags::from(power_level));
+    match power_level {
+        PowerLevel::Low => {
+            gamepad.battery = u8::MAX / 3;
+        },
+        PowerLevel::Medium => {
+            gamepad.battery = u8::MAX / 3 * 2;
+        },
+        PowerLevel::Full => {
+            gamepad.battery = u8::MAX;
+        },
+        _ => {
+            gamepad.battery = 0;
+        },
+    }
+
+    // Read gamepad buttons.
+    // Temporary variable used to avoid an unaligned access.
+    let mut buttons = PinputGamepadButtons::default();
+    for button in SDL_GAME_CONTROLLER_BUTTONS {
+        if game_controller.button(button) {
+            if let Ok(button) = PinputGamepadButtons::try_from(button) {
+                buttons.insert(button);
+            }
+        }
+    }
+    gamepad.buttons = buttons;
+
+    // Read gamepad axes (including triggers).
+    // Note that SDL Y axes are upside-down compared to XInput:
+    // <https://github.com/libsdl-org/SDL/blob/9130f7c/src/joystick/windows/SDL_xinputjoystick.c#L462-L465>
+    gamepad.left_stick_x = game_controller.axis(Axis::LeftX);
+    gamepad.left_stick_y = !game_controller.axis(Axis::LeftY);
+    gamepad.right_stick_x = game_controller.axis(Axis::RightX);
+    gamepad.right_stick_y = !game_controller.axis(Axis::RightY);
+    gamepad.left_trigger = (game_controller.axis(Axis::TriggerLeft) / 0x81) as u8;
+    gamepad.right_trigger = (game_controller.axis(Axis::TriggerRight) / 0x81) as u8;
 
     Ok(())
 }
