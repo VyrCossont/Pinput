37be0d90bdc829ec34cbad3db89f953198e093b9
Vyr Cossont <VyrCossont@users.noreply.github.com>
2021-11-19 23:57:50 -0800

    PICO-8: fixed release script for .p8.png
    This currently can't be automated. There's a Lexaloffle BBS thread asking for it: https://www.lexaloffle.com/bbs/?tid=38271

--- release/release-pico8.sh
+++ release/release-pico8.sh
@@ -5,7 +5,7 @@ set -eu
 # Build the .p8.png and web demos of `pinput_tester.png`.
 
 # https://stackoverflow.com/a/29835459
-release_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+release_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 
 pico8_src_dir="${release_dir}/../PICO-8"
 docs_dir="${release_dir}/../docs"
@@ -24,9 +24,16 @@ if [ -z "${pico8_path}" ]; then
 fi
 
 # Export .p8.png version.
+# Has to be done manually.
+echo 'PICO-8 will launch.'
+echo 'Press Esc.'
+echo 'Type: save pinput_tester.p8.png'
+echo 'Press Return.'
+echo 'Type: shutdown'
+echo 'Press Return.'
 "${pico8_path}" \
   "${pico8_src_dir}/pinput_tester.p8" \
-  -export "${pico8_src_dir}/pinput_tester.p8.png"
+  -root_path "${pico8_src_dir}"
 
 # Export HTML version.
 "${pico8_path}" \
