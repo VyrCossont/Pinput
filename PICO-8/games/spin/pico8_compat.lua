fda749211cd576ef26b4324fb274e59fb647bd82
Vyr Cossont <VyrCossont@users.noreply.github.com>
2021-12-12 17:57:16 -0800

    Spin: implement tostr()

--- PICO-8/games/spin/pico8_compat.lua
+++ PICO-8/games/spin/pico8_compat.lua
@@ -1,21 +1,21 @@
--- import these functions to make Lua 5.2 expose a more PICO-8-like API.
+-- import these functions to make Lua 5.4 expose a more PICO-8-like API.
 -- intended for getting some PICO-8 code to run in LuaUnit.
 
 -- not documented in PICO-8 manual
--- https://www.lua.org/manual/5.2/manual.html#pdf-table.pack
+-- https://www.lua.org/manual/5.4/manual.html#pdf-table.pack
 pack = table.pack
 
 -- not documented in PICO-8 manual
--- https://www.lua.org/manual/5.2/manual.html#pdf-table.unpack
+-- https://www.lua.org/manual/5.4/manual.html#pdf-table.unpack
 unpack = table.unpack
 
 -- https://www.lexaloffle.com/dl/docs/pico-8_manual.html#CHR
--- https://www.lua.org/manual/5.2/manual.html#pdf-string.char
+-- https://www.lua.org/manual/5.4/manual.html#pdf-string.char
 chr = string.char
 
 -- third arg has a different meaning from vanilla string.byte
 -- https://www.lexaloffle.com/dl/docs/pico-8_manual.html#ORD
--- https://www.lua.org/manual/5.2/manual.html#pdf-string.byte
+-- https://www.lua.org/manual/5.4/manual.html#pdf-string.byte
 function ord(str, index, num_results)
  if type(str) ~= 'string' then
   return nil
@@ -35,7 +35,7 @@ end
 -- vs. if provided (and thus still nil)
 -- but the number of function arguments is different.
 -- https://www.lexaloffle.com/dl/docs/pico-8_manual.html#SUB
--- https://www.lua.org/manual/5.2/manual.html#pdf-string.sub
+-- https://www.lua.org/manual/5.4/manual.html#pdf-string.sub
 function sub(str, pos0, ...)
  local opt_args = pack(...)
  local pos1 = ...
@@ -43,4 +43,62 @@ function sub(str, pos0, ...)
   pos1 = pos0
  end
  return string.sub(str, pos0, pos1)
+end
+
+-- https://www.lexaloffle.com/dl/docs/pico-8_manual.html#TOSTR
+-- https://www.lexaloffle.com/dl/docs/pico-8_manual.html#Types_and_assignment
+-- https://www.lua.org/manual/5.4/manual.html#pdf-tostring
+-- https://www.lua.org/manual/5.4/manual.html#pdf-string.format
+-- https://www.lua.org/manual/5.4/manual.html#pdf-math.modf
+function tostr(...)
+ local args = pack(...)
+ if args.n == 0 then
+  return ''
+ end
+ local val, format_flags = ...
+ if format_flags == nil then
+  format_flags = 0
+ elseif format_flags == false then
+  format_flags = 0
+ elseif format_flags == true then
+  format_flags = 1
+ end
+ local type_val = type(val)
+
+ -- not affected by either format flag
+ if type_val == 'nil' or type_val == 'thread' then
+  return '[' .. type_val .. ']'
+ elseif type_val == 'string' then
+  return val
+ elseif type_val == 'boolean' then
+  return tostring(boolean)
+ end
+
+ -- affected by hex flag
+ local hex = (format_flags >> 0) & 1 == 1
+ if type_val == 'function' or type_val == 'table' then
+  if hex then
+   return tostring(val)
+  else
+   return '[' .. type_val .. ']'
+  end
+ end
+
+ -- at this point we should be down to numbers
+ assert(type_val == 'number')
+ local shift16 = (format_flags >> 1) & 1 == 1
+ if not hex and not shift16 then
+  return tostring(val)
+ end
+ val = math.modf(val * 2^16)
+ if not hex and shift16 then
+  return tostring(val)
+ end
+ val = string.format("%016x", val)
+ if shift16 then
+  val = string.sub(val, -8)
+ else
+  val = string.sub(val, -8, -5) .. '.' .. string.sub(val, -4)
+ end
+ return '0x' .. val
 end
--- PICO-8/games/spin/test.lua
+++ PICO-8/games/spin/test.lua
@@ -12,43 +12,78 @@ test_pico8_compat = {}
 function test_pico8_compat.test_chr ()
  luaunit.assertEquals(
    chr(64),
-   "@"
+   '@'
  )
  luaunit.assertEquals(
    chr(104, 101, 108, 108, 111),
-   "hello"
+   'hello'
  )
 end
 
 function test_pico8_compat.test_ord()
  luaunit.assertEquals(
-   ord("@"),
+   ord('@'),
    64
  )
  luaunit.assertEquals(
-   ord("123", 2),
+   ord('123', 2),
    50
  )
  -- /!\ this example is wrong in the PICO-8 manual
  luaunit.assertEquals(
-   table.pack(ord("1234", 2, 3)),
+   table.pack(ord('1234', 2, 3)),
    {50, 51, 52, n = 3}
  )
 end
 
 function test_pico8_compat.test_sub()
- local s = "the quick brown fox"
+ local s = 'the quick brown fox'
  luaunit.assertEquals(
    sub(s, 5, 9),
-   "quick"
+   'quick'
  )
  luaunit.assertEquals(
    sub(s, 5),
-   "quick brown fox"
+   'quick brown fox'
  )
  luaunit.assertEquals(
    sub(s, 5, _),
-   "q"
+   'q'
+ )
+end
+
+function test_pico8_compat.test_tostr()
+ luaunit.assertEquals(
+   tostr(-32768, true),
+   '0x8000.0000'
+ )
+ luaunit.assertEquals(
+   tostr(32767.99999, true),
+   '0x7fff.ffff'
+ )
+ luaunit.assertEquals(
+   tostr(),
+   ''
+ )
+ luaunit.assertEquals(
+   tostr(nil),
+   '[nil]'
+ )
+ luaunit.assertEquals(
+   tostr(17),
+   '17'
+ )
+ luaunit.assertEquals(
+   tostr(17, 0x01),
+   '0x0011.0000'
+ )
+ luaunit.assertEquals(
+   tostr(17, 0x03),
+   '0x00110000'
+ )
+ luaunit.assertEquals(
+   tostr(17, 0x02),
+   '1114112'
  )
 end
 
@@ -57,11 +92,11 @@ require 'format'
 test_format = {}
 
 function test_format.test_format_is_id()
- luaunit.assertTrue(format_is_id("x"))
- luaunit.assertTrue(format_is_id("_"))
- luaunit.assertTrue(format_is_id("x1"))
- luaunit.assertTrue(format_is_id("_1"))
- luaunit.assertFalse(format_is_id("1"))
+ luaunit.assertTrue(format_is_id('x'))
+ luaunit.assertTrue(format_is_id('_'))
+ luaunit.assertTrue(format_is_id('x1'))
+ luaunit.assertTrue(format_is_id('_1'))
+ luaunit.assertFalse(format_is_id('1'))
 end
 
 os.exit(luaunit.LuaUnit.run())
